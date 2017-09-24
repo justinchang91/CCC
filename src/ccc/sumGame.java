e5f0acc589f1bf37964b720c611f9269e1906e7c
justi <justi@JUSTINS-PC>
2017-09-23 20:46:20 -0400

    This is a project folder for when i practice CCC questions. I didnt do anything yet because im too tilted

---
+++ src/ccc/sumGame.java
@@ -0,0 +1,25 @@
+/*
+Sept 23, 2017
+Competition: 2017
+
+The season lasted for N days. 
+Both teams played exactly one game on each day.
+
+
+ */
+package ccc;
+
+/**
+ *
+ * @author justi
+ */
+public class sumGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
